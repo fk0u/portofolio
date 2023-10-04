984d2d3f8d2b3975cddae47cc6a6d8bcca9b8fa4
KOU <142812925+fk0u@users.noreply.github.com>
2023-10-05 07:40:45 +0800

    Add files via upload

---
+++ assets/js/hire.js
@@ -0,0 +1,5 @@
+function redirectToWhatsApp() {
+    var phoneNumber = "62818703303"; // Ganti dengan nomor WhatsApp yang sesuai
+    var whatsappURL = "https://wa.me/" + phoneNumber;
+    window.location.href = whatsappURL;
+}
