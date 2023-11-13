8968b40357be212de6e702d4c356f47f0d77e92b
KOU <142812925+fk0u@users.noreply.github.com>
2023-11-13 19:46:17 +0800

    Delete assets directory

--- assets/js/hire.js
+++
@@ -1,5 +0,0 @@
-function redirectToWhatsApp() {
-    var phoneNumber = "62818703303"; // Ganti dengan nomor WhatsApp yang sesuai
-    var whatsappURL = "https://wa.me/" + phoneNumber;
-    window.location.href = whatsappURL;
-}
